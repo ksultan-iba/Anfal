648723a600af5a7b9f2bc5ddf57517d369f17e10
ksultan-iba <31192350+ksultan-iba@users.noreply.github.com>
2017-09-14 19:06:07 +0500

    Update Main.java

--- q2/src/com/company/Main.java
+++ q2/src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 
 import java.util.Scanner;
-
+//javadocs??
 public class Main {
 
+    //javadocs??
     public static void main(String[] args) {
 
         Scanner a = new Scanner (System.in);
 System.out.println("Enter a number");
 
   int b = a.nextInt();
+        //what if b is negative?
   int factorial = 1 ;
   for (int i = 1 ;i<=b ; i++ ){
   factorial = factorial*i;
